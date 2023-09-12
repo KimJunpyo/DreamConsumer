4a6b08dfa32955317d95483ebb1e9e2589e4ed3c
coaudtn0276 <coaudtn0276@gmail.com>
2023-09-12 16:07:19 +0900

    폴더구조

---
+++ client_dc/src/app/detail/group/money/_components/push.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Push() {
+  return (
+    <div>
+      <div onClick={() => {}}>버튼</div>
+    </div>
+  );
+}
--- client_dc/src/app/detail/group/money/page.tsx
+++ client_dc/src/app/detail/group/money/page.tsx
@@ -1,3 +1,10 @@
+import Push from './_components/push';
+
 export default function Money() {
-  return <div>/money 페이지</div>;
+  return (
+    <div>
+      /money 페이지
+      <Push />
+    </div>
+  );
 }
