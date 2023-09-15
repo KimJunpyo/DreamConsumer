39d67329e34bc16dc3ac96fc9ec1e6d48c3b805e
hyeincha <runprogrmm@gmail.com>
2023-09-15 10:38:52 +0900

    DREAM-11 feat: 체크박스 구현

---
+++ client_dc/src/components/Checkbox.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+type Props = {
+  children: string;
+  value: boolean;
+  inputName?: string;
+  handler: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export default function Checkbox({ children, value, inputName, handler }: Props) {
+  return (
+    <div className='flex items-center gap-1 my-1'>
+      <input
+        type='checkbox'
+        className='appearance-none w-4 h-4 border border-blue-primary rounded-sm bg-white checked:bg-blue-primary'
+        checked={value}
+        name={inputName}
+        onChange={handler}
+      />
+      <label className='text-grey-text text-sm'>{children}</label>
+    </div>
+  );
+}
