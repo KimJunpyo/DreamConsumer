d507b0b6ba36e6bdd3485c9ac4419480f56ae9f9
KimJunpyo <bono1725@naver.com>
2023-09-25 08:03:24 +0900

    DREAM-9 feat: Radio
    - Radio 기능을 위한 props 추가
    - img 이름 변경 및 index.ts 추가

---
+++ client_dc/public/image/index.ts
@@ -0,0 +1,13 @@
+export { default as alarm } from './alarm.svg';
+export { default as dashLine } from './dashLine.svg';
+export { default as delete } from './delete.svg';
+export { default as hamburger } from './hamburger.svg';
+export { default as introBG } from './introBG.svg';
+export { default as moon } from './moon.svg';
+export { default as plus } from './plus.svg';
+export { default as profile } from './profile.svg';
+export { default as refresh } from './refresh.svg';
+export { default as vercel } from './dashLine.svg';
+export { default as darkDownArrow } from './darkDownArrow.svg';
+export { default as downArrow } from './downArrow.svg';
+export { default as whiteDownArrow } from './whiteDownArrow.svg';
--- client_dc/src/components/Dropdown.tsx
+++ client_dc/src/components/Dropdown.tsx
@@ -2,17 +2,17 @@
 
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
-import DownArrow from '~/image/DownArrow.svg';
-import DarkDownArrow from '~/image/DarkDownArrow.svg';
-import WhiteDownArrow from '~/image/whiteDownArrow.svg';
+import { downArrow, darkDownArrow, whiteDownArrow } from '~/image';
 
 type Props = {
-  list: number[] | string[];
+  list: string[];
   prevIcon?: boolean;
   borderless?: boolean;
   filled?: boolean;
   width: string;
-  setState: React.Dispatch<React.SetStateAction<number | string>>;
+  setState: React.Dispatch<React.SetStateAction<string>>;
+  handler?: () => void;
+  radio?: boolean;
 };
 
 /** using example */
@@ -35,11 +35,22 @@ type Props = {
       /> */
 }
 
-export default function Dropdown({ list, prevIcon, borderless, filled, width, setState }: Props) {
+export default function Dropdown({
+  list,
+  prevIcon,
+  borderless,
+  filled,
+  width,
+  setState,
+  handler,
+  radio,
+}: Props) {
   const [clickDropdown, setClickDropdown] = useState<boolean>(false);
   const [showOptions, setShowOptions] = useState<boolean>(false);
   const [selectedIdx, setSelectedIdx] = useState<number>(0);
   const [showShadow, setShowShadow] = useState<boolean>(false);
+
+  const REMOVED_OPTIONS = ['매주', '매월'];
 
   const borderlessClass = `${clickDropdown ? 'border-blue-primary' : 'border-transparent'}`;
   const filledClass = `border-b-transparent text-white ${
@@ -49,6 +60,7 @@ export default function Dropdown({ list, prevIcon, borderless, filled, width, se
   const handleClickOption = (idx: number) => {
     setSelectedIdx(idx);
     setState(list[idx]);
+    setClickDropdown(false);
   };
 
   useEffect(() => {
@@ -61,22 +73,34 @@ export default function Dropdown({ list, prevIcon, borderless, filled, width, se
     }
   }, [clickDropdown]);
 
+  useEffect(() => {
+    if (!radio) {
+      setClickDropdown(false);
+      setSelectedIdx(0);
+    }
+  }, [radio]);
+
   return (
     <div
-      className={`relative font-neb text-xs rounded-lg origin-top text-blue-primary ${
+      className={`relative bg-white font-neb text-xs rounded-lg origin-top text-blue-primary ${
         clickDropdown && 'rounded-b-none'
       } ${width && width} ${showShadow ? 'animate-show-shadow' : ''}`}
-      onClick={() => setClickDropdown(!clickDropdown)}
+      onClick={handler}
     >
       <div
-        className={`duration-100 border border-blue-primary rounded-lg relative ${
-          clickDropdown ? 'border-b-transparent rounded-b-none text-grey-text ' : ''
-        }
+        className={`duration-100 h-full border border-blue-primary rounded-lg relative ${
+          radio && 'bg-grey-text'
+        } ${clickDropdown ? 'border-b-transparent rounded-b-none text-grey-text ' : ''}
          ${borderless ? borderlessClass : ''} ${filled ? filledClass : ''}`}
+        onClick={() => {
+          setClickDropdown(!clickDropdown);
+        }}
       >
-        <div className='flex h-7 items-center justify-center'>{list[selectedIdx]}</div>
+        <div className='flex h-full items-center justify-center'>
+          {selectedIdx === 0 ? list[0] : list[selectedIdx]}
+        </div>
         <Image
-          src={filled ? WhiteDownArrow : clickDropdown ? DarkDownArrow : DownArrow}
+          src={filled ? whiteDownArrow : clickDropdown ? darkDownArrow : downArrow}
           className={`absolute top-1/2 -translate-y-1/2 ${prevIcon ? 'left-2' : 'right-2'}`}
           width={20}
           height={20}
@@ -87,19 +111,22 @@ export default function Dropdown({ list, prevIcon, borderless, filled, width, se
       <div
         className={`${showOptions ? 'block' : 'hidden'}  ${
           showShadow ? 'animate-show-shadow' : ''
-        } ${width && width} absolute z-30 border rounded-lg border-t-0 rounded-t-none origin-top ${
+        } w-full absolute z-30 border rounded-lg border-t-0 rounded-t-none origin-top ${
           filled ? 'bg-grey-text border-grey-text' : 'bg-white border-blue-primary'
         } ${clickDropdown ? 'animate-ease-down' : 'animate-ease-up'}`}
       >
         {list.map(
           (option, idx) =>
-            idx !== selectedIdx && (
+            idx !== selectedIdx &&
+            !REMOVED_OPTIONS.includes(option) && (
               <option
                 className={`flex h-7 items-center justify-center text-blue-primary ${
                   filled ? 'text-white' : 'text-blue-primary'
                 }`}
                 key={idx}
-                onClick={() => handleClickOption(idx)}
+                onClick={() => {
+                  handleClickOption(idx);
+                }}
               >
                 {option}
               </option>
