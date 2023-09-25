9f17af8d21f673466ebda824750ff43251dbcd79
coaudtn0276 <coaudtn0276@gmail.com>
2023-09-25 14:59:36 +0900

    NavBar수정

--- client_dc/src/components/NavBar.tsx
+++ client_dc/src/components/NavBar.tsx
@@ -16,19 +16,22 @@ export default function Nav() {
   };
 
   return (
-    <div className='h-[84px] bg-blue-primary flex justify-between items-center'>
-      <Image src={Menu} alt='menu' className='ml-4' onClick={menuHandler} />
+    <>
+      <div className='h-[84px] bg-blue-primary flex justify-between items-center'>
+        <Image src={Menu} alt='menu' className='ml-4' onClick={menuHandler} />
+
+        <div className='font-nb text-base text-white'>
+          <span className='text-mint'>D</span>ream
+          <span className='text-mint ml-2'>C</span>onsumer
+        </div>
+        <div className='flex flex-row mr-3'>
+          <Image src={Alarm} alt='menu' className='mr-1' />
+          <Image src={Profile} alt='menu' />
+        </div>
+      </div>
       <div>
         <NavMenu menuValue={menuValue} handler={menuHandler} />
       </div>
-      <div className='font-nb text-base text-white'>
-        <span className='text-mint'>D</span>ream
-        <span className='text-mint ml-2'>C</span>onsumer
-      </div>
-      <div className='flex flex-row mr-3'>
-        <Image src={Alarm} alt='menu' className='mr-1' />
-        <Image src={Profile} alt='menu' />
-      </div>
-    </div>
+    </>
   );
 }
