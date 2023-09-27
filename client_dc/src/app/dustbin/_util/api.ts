19c1a85a747e61f530b788c78de01f94ed5d8f60
hyeincha <runprogrmm@gmail.com>
2023-09-27 15:59:02 +0900

    DREAM-17 feat: 휴지통 api 함수 생성

--- client_dc/src/app/dustbin/_util/api.ts
+++ client_dc/src/app/dustbin/_util/api.ts
@@ -1,12 +1,43 @@
 import { extendedFetch } from '@/common/api/instance';
 
+export const getDustbin = async (id: number, searchParams: string) => {
+  const res = await extendedFetch(`/trashcans/${id}?${searchParams}`, { method: 'GET' });
+
+  if (!res.ok) {
+    throw new Error('Failed to get dustbin');
+  }
+
+  return res.json();
+};
+
 export const deleteAll = async () => {
   const res = await extendedFetch('/trashcans', { method: 'DELETE' });
 
   if (!res.ok) {
-    // response body 확인 후 수정
     throw new Error('Failed to delete all');
   }
 
   return res.json();
+};
+
+export const deleteItems = async (items: number[]) => {
+  const promises = items.map(async (id) => {
+    const endpoint = `/trashcans/${id}`;
+    const res = await extendedFetch(endpoint, { method: 'DELETE' });
+    return res.json();
+  });
+
+  const res = await Promise.allSettled(promises);
+  console.log(res);
+};
+
+export const restoreItems = async (items: number[]) => {
+  const promises = items.map(async (id) => {
+    const endpoint = `/trashcans/${id}`;
+    const res = await extendedFetch(endpoint, { method: 'PUT' });
+    return res.json();
+  });
+
+  const res = await Promise.allSettled(promises);
+  console.log(res);
 };
